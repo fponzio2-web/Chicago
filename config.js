a429dcef480faed31d38be10d974c52492a89782
fponzio2-web <fponzio2@gmail.com>
2026-09-21 18:20:48 -0500

    Update config.js
    Correcao

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://lhkkqyisyqgrvebdmnoy.supabase.co",   // ex.: https://abcdxyz.supabase.co
   supabaseKey: "sb_publishable_iP2YaLAeAw4XAAsDEYuT7w_WNCEL_IG",  // anon ou publishable. Nunca use a secret / service_role
-  passcodeHash: "liljamesgostadecriancas"
+  passcodeHash: "f27e7ffa5327bfe8f80a0583b7c886f240554cddd52be3c140aabe000bec9f62"
 };
